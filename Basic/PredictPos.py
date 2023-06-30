9ee22440444db4a923b08113daef8994f90bbafc
RemiLoka <remi.lokajczyk@ensea.fr>
2023-06-30 17:36:56 +0300

    NotFinishApproach

--- Basic/PredictPos.py
+++ Basic/PredictPos.py
@@ -8,3 +8,17 @@ def FuturPos(vehicle, dt = 1):
     pos = traci.vehicle.getPosition(vehicle)
     futurPos = [np.cos(angle)*((1/2)*accel*(dt**2) + speed*dt) + pos[0],np.sin(angle)*((1/2)*accel*(dt**2) + speed*dt) + pos[1]]
     return futurPos
+
+def Direction(vehicle, posTL):
+    angle = traci.vehicle.getAngle(vehicle)
+    pos = traci.vehicle.getPosition(vehicle)
+    d = np.sqrt((posTL[0]-pos[0])**2+(posTL[1]-pos[1])**2)
+    teta = np.arccos((posTL[0]-pos[0])/d)
+    if posTL[1] > pos[1]:
+        teta = (teta*180)/np.pi
+    else:
+        teta = 360 - (teta*180)/np.pi
+    if np.abs(teta - angle)< 90:
+        return "en approche"
+    else:
+        return "s'éloigne"
