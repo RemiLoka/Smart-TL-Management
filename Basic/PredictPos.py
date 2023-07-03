833d002044c12ec3100414b5c06ffb96bc0b5ac0
RemiLoka <remi.lokajczyk@ensea.fr>
2023-07-03 13:03:12 +0300

    Direction

--- Basic/PredictPos.py
+++ Basic/PredictPos.py
@@ -10,15 +10,45 @@ def FuturPos(vehicle, dt = 1):
     return futurPos
 
 def Direction(vehicle, posTL):
+    dir = 'TL'
     angle = traci.vehicle.getAngle(vehicle)
     pos = traci.vehicle.getPosition(vehicle)
-    d = np.sqrt((posTL[0]-pos[0])**2+(posTL[1]-pos[1])**2)
-    teta = np.arccos((posTL[0]-pos[0])/d)
-    if posTL[1] > pos[1]:
-        teta = (teta*180)/np.pi
-    else:
-        teta = 360 - (teta*180)/np.pi
-    if np.abs(teta - angle)< 90:
-        return "en approche"
-    else:
-        return "s'éloigne"
+    dx = pos[0]-posTL[0]
+    dy = pos[1]-posTL[1]
+    if ((dx >= 0) and (dy >= 0)):
+        if 0 <= angle <= 90:
+            exit = "moves away"
+            if angle <= 45:
+                dir = 'north'
+            else:
+                dir = 'est'
+        else:
+            exit = "in approach"
+    elif ((dx <= 0) and (dy >= 0)):
+        if 90 <= angle <= 180:
+            exit = "moves away"
+            if angle <= 135:
+                dir = 'est'
+            else:
+                dir = 'south'
+        else:
+            exit = "in approach"
+    elif ((dx <= 0) and (dy <= 0)):
+        if 180 <= angle <= 270:
+            exit = "moves away"
+            if angle <= 225:
+                dir = 'south'
+            else:
+                dir = 'west'
+        else:
+            exit = "in approach"
+    elif ((dx >= 0) and (dy <= 0)):
+        if 270 <= angle <= 360:
+            exit = "moves away"
+            if angle <= 315:
+                dir = 'west'
+            else:
+                dir = 'north'
+        else:
+            exit = "in approach"
+    return exit +' to '+ dir + ' with ' + str(angle) + ' degree'
